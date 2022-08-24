66c2b83b0f8085ebc7f115f5a19bd4038a60086c
chrisloarryn <chrisloarryn@gmail.com>
2022-08-24 12:22:06 -0400

    chore: add usecases and domain for products api management

--- go.mod
+++ go.mod
@@ -5,22 +5,45 @@ go 1.18
 require github.com/gin-gonic/gin v1.8.1
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/bluele/gcache v0.0.0-20190518031135-bc40bd653833 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-kit/kit v0.10.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/gorilla/mux v1.7.3 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/oklog/oklog v0.3.2 // indirect
+	github.com/oklog/run v1.0.0 // indirect
+	github.com/opentracing/opentracing-go v1.1.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.3.0 // indirect
+	github.com/prometheus/client_model v0.1.0 // indirect
+	github.com/prometheus/common v0.7.0 // indirect
+	github.com/prometheus/procfs v0.0.8 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
+	github.com/validatecl/go-microservices-commons v1.0.14 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
-	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
 	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
 	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/grpc v1.26.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/core/usecases/products_finder_by_sku_test.go
@@ -0,0 +1,84 @@
+package usecases_test
+
+import (
+	"context"
+	"fmt"
+	"github.com/golang/mock/gomock"
+	"github.com/skus-finder-psql/internal/core/domain/products"
+	"github.com/skus-finder-psql/internal/core/domain/products/productsmocks"
+	"github.com/skus-finder-psql/internal/core/usecases"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	commons "github.com/validatecl/go-microservices-commons"
+	"testing"
+)
+
+func TestFinderProductBySKU_Execute_ShouldReturnsAProductData(t *testing.T) {
+	t.Log("Should returns a product from his SKU")
+	// Setup
+	controller := gomock.NewController(t)
+
+	productSKU := "213321123123"
+	productResult := products.Product{
+		ID:             123,
+		Sku:            "213321123123",
+		Name:           "product",
+		Size:           commons.StringPointer("XL"),
+		Price:          1000.01,
+		PrincipalImage: commons.StringPointer("http://image.png"),
+		OtherImages: []*string{
+			commons.StringPointer("http://other_image.png"),
+		},
+	}
+
+	repository := productsmocks.NewMockRepository(controller)
+	repository.EXPECT().FindProductBySKU(gomock.Any(), productSKU).Return(productResult, nil).Times(1)
+
+	operation := usecases.NewFinderProductsBySKU(repository)
+
+	// Execute
+	result, err := operation.Execute(context.TODO(), productSKU)
+
+	// Verify
+	require.NoError(t, err)
+	assert.Equal(t, productResult, result)
+}
+
+func TestFinderProductBySKU_Execute_ShouldReturnsAnErrorFromRepository(t *testing.T) {
+	t.Log("Should returns an error from repository")
+	// Setup
+	controller := gomock.NewController(t)
+
+	productSKU := "123"
+	customError := fmt.Errorf("this is a custom error")
+
+	repository := productsmocks.NewMockRepository(controller)
+	repository.EXPECT().FindProductBySKU(gomock.Any(), productSKU).Return(products.Product{}, customError).Times(1)
+
+	operation := usecases.NewFinderProductsBySKU(repository)
+
+	// Execute
+	result, err := operation.Execute(context.TODO(), productSKU)
+
+	// Verify
+	require.Error(t, err, customError.Error())
+	assert.Equal(t, products.Product{}, result)
+}
+
+func TestFinderProductBySKU_Execute_ShouldReturnsAnErrorForInvalidID(t *testing.T) {
+	t.Log("Should returns an error for invalid ID")
+	// Setup
+	controller := gomock.NewController(t)
+
+	productSKU := ""
+	repository := productsmocks.NewMockRepository(controller)
+
+	operation := usecases.NewFinderProductsBySKU(repository)
+
+	// Execute
+	result, err := operation.Execute(context.TODO(), productSKU)
+
+	// Verify
+	require.Error(t, err, fmt.Sprintf("Invalid ID: %s", productSKU))
+	assert.Equal(t, products.Product{}, result)
+}
